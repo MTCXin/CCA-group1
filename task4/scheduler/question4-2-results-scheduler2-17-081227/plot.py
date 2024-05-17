d8e3616623d7ce7d9038638be1ebb53d57ff6697
MTCXin <mtcxnbss@gmail.com>
2024-05-17 12:45:09 +0200

    new plot v2

--- task4/scheduler/question4-2-results-scheduler2-17-081227/plot.py
+++ task4/scheduler/question4-2-results-scheduler2-17-081227/plot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import datetime
 import numpy as np
 
+# log.txt
 with open('log.txt') as f:
     logs = f.readlines()
 
@@ -10,7 +10,7 @@ with open('log.txt') as f:
 with open('measure.txt') as f:
     measures = f.readlines()
 
-# log.txt
+# parse log.txt
 scheduler_start_time = None
 scheduler_end_time = None
 memcached_cores = []
@@ -44,7 +44,7 @@ for log in logs:
         job = parts[2]
         batch_jobs[job].append((timestamp, 'end'))
 
-# 解析measure.txt
+# parse measure.txt
 intervals = int(measures[0].split('=')[1].strip())
 timestamp_start = int(measures[1].split(':')[1].strip())
 timestamp_end = int(measures[2].split(':')[1].strip())
@@ -59,40 +59,50 @@ for measure in measures[4:]:
         p95_latency.append(float(parts[11]))
         qps.append(float(parts[15]))
 
-
+# align
 time_axis = [scheduler_start_time + datetime.timedelta(seconds=i * interval_duration) for i in range(intervals)]
 
+
 time_axis_start_time = scheduler_start_time
+time_axis_end_time = scheduler_end_time
 time_axis = [(t - time_axis_start_time).total_seconds() for t in time_axis]
 
-# plot A
+# A: p95 latency QPS
 fig, (axA, axB, axC) = plt.subplots(3, 1, sharex=True, figsize=(12, 18))
 
 axA.plot(time_axis, [p / 1000 for p in p95_latency], color='blue', label='p95 Latency (ms)', linewidth=2)
 axA.axhline(y=1, color='r', linestyle='--', label='Target 1ms Latency')
 axA.set_ylabel('p95 Latency (ms)')
-axA.set_ylim(bottom=0)
+axA.set_ylim(0, 2.5)
 axA.legend(loc='upper left')
 
 axA2 = axA.twinx()
 axA2.fill_between(time_axis, 0, [q / 1000 for q in qps], color='green', alpha=0.3, label='QPS (kQPS)')
 axA2.set_ylabel('QPS (kQPS)')
+axA2.set_ylim(0, 100)
 axA2.legend(loc='upper right')
 
-# plot B
+# B: memcached CPU QPS
 core_times = [(t[0] - time_axis_start_time).total_seconds() for t in memcached_cores]
 core_values = [t[1] for t in memcached_cores]
 
-axB.stem(core_times, core_values, linefmt='orange', markerfmt='D', basefmt=' ', label='Memcached CPU Cores')
+
+for i in range(len(core_times) - 1):
+    axB.plot([core_times[i], core_times[i + 1]], [core_values[i], core_values[i]], color='orange')
+axB.plot([core_times[-1], (time_axis_end_time - time_axis_start_time).total_seconds()], [core_values[-1], core_values[-1]], color='orange')
+
 axB.set_ylabel('Memcached CPU Cores')
+axB.set_ylim(0, 4)
+axB.set_yticks(range(0, 5))  
 axB.legend(loc='upper left')
 
 axB2 = axB.twinx()
 axB2.fill_between(time_axis, 0, [q / 1000 for q in qps], color='green', alpha=0.3, label='QPS (kQPS)')
 axB2.set_ylabel('QPS (kQPS)')
+axB2.set_ylim(0, 100)
 axB2.legend(loc='upper right')
 
-# plot C
+# C: batch job
 job_colors = {
     'blackscholes': '#CCA000', 'canneal': '#CCCCAA', 'dedup': '#CCACCA', 
     'ferret': '#AACCCA', 'freqmine': '#0CCA00', 'radix': '#00CCA0', 'vips': '#CC0A00'
@@ -101,19 +111,21 @@ job_colors = {
 for job, events in batch_jobs.items():
     for i in range(len(events)):
         start_event = events[i]
-        if start_event[1] == 'start':
-            for j in range(i+1, len(events)):
+        if start_event[1] in ['start', 'unpause']:
+            for j in range(i + 1, len(events)):
                 end_event = events[j]
-                if end_event[1] in ['pause', 'unpause', 'end']:
+                if end_event[1] in ['pause', 'end']:
                     start_time_sec = (start_event[0] - time_axis_start_time).total_seconds()
                     end_time_sec = (end_event[0] - time_axis_start_time).total_seconds()
                     axC.barh(job, end_time_sec - start_time_sec, left=start_time_sec, color=job_colors[job], edgecolor='black')
-                    if end_event[1] == 'end':
-                        break
+                    break
 
 axC.set_yticks(list(batch_jobs.keys()))
 axC.set_xlabel('Time (s)')
 axC.set_ylabel('Batch Jobs')
 axC.grid(True)
+
+
+axC.set_xlim(0, (time_axis_end_time - time_axis_start_time).total_seconds())
 
 plt.show()
