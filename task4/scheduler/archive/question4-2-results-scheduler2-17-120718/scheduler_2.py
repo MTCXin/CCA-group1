952627885bcb4da6f8175bac1162a827e35e6aff
andreak <keuscha@ethz.ch>
2024-05-17 13:50:56 +0200

    add results, fix plot

---
+++ task4/scheduler/archive/question4-2-results-scheduler2-17-120718/scheduler_2.py
@@ -0,0 +1,440 @@
+import psutil
+import scheduler_logger as slogger
+import subprocess
+import sys
+import docker
+import time
+
+# POLICY
+# We have 2 list of jobs: small and large ones
+# if the load memcached is low:
+#       - run memcached on cores 0
+#       - run a large job on cores 1,2,3 
+#       - pause small jobs 
+#       - if all large jobs finished, we run a small job instead
+#       
+# if the load on memcaches is high:
+#       - run memcacched on cores 0,1
+#       - run a small job on cores 2,3
+#       - pause large jobs
+#
+# whether the load on memcached is low or high is determined by monitoring the CPU utilization of the memcached cores.
+
+
+SLEEP_TIME = 0.2 # TODO: find suitable value or remove sleep
+
+THRESHOLD_HIGH = 50
+THRESHOLD_LOW = 30
+# TOLERANCE = 10
+
+class BatchJob(object):
+    name = ""
+    num_threads = 4 # default, can be overriden
+    image = ""
+    command = ""
+    container = None # will be set once the container has been created
+    cores = [] # will be set when the job is started and is updated dynamically
+    has_finished_ = False # will be set once the container finished and was removed
+    start_time = None
+    runtime = 0.0
+
+    def is_running(self):
+        if self.container is None:
+            return False
+        self.container.reload()
+        return self.container.status in ["created", "running", "restarting"]
+
+    def has_finished(self):
+        if self.container is None:
+            return self.has_finished_
+        self.container.reload()
+        return self.container.status in ["exited"] 
+    
+    def is_newly_finished(self):
+        return (not self.container is None) and self.container.status in ["exited"] 
+
+    def is_paused(self):
+        if self.container is None:
+            return False
+        self.container.reload()
+        return self.container.status in ["paused"] 
+    
+    def is_new(self):
+        return self.container is None and not self.has_finished_
+    
+    def reload_stats(self):
+        if self.container is not None:
+            self.container.reload()
+    
+    def get_status(self):
+        if self.has_finished_:
+            return "exited"
+        if self.container is None:
+            return "not started"
+        self.container.reload()
+        return self.container.status
+
+    def __str__(self):
+        return f"{self.name} ({self.image}): \n\tcores = {self.cores}\n\tcommand = {self.command}\n\tstatus = {self.get_status()}" 
+
+    def start_job(self, docker_client, cpu_set, logger):
+        self.cores = cpu_set
+        container = docker_client.containers.run(cpuset_cpus=",".join([str(c) for c in cpu_set]),
+                                               name=self.name,
+                                               detach=True,
+                                               auto_remove=False,
+                                               image=self.image,
+                                               command=self.command)
+        logger.job_start(self.name, [str(c) for c in cpu_set], self.num_threads)
+        self.start_time = time.time()
+        # print(f"Start {self}")
+        self.container = container
+
+    def pause_job(self, logger):
+        if self.container is None:
+            return False
+        self.container.reload()
+        if not self.is_running():
+            # print(f"WARN: Cannot pause {self.name} because job is not running.")
+            return False
+        try:
+            self.container.pause()
+            logger.job_pause(self.name)
+            self.runtime = self.runtime + (time.time() - self.start_time)
+            # print(f"Pause {self}")
+            return True
+        except:
+            print(f"ERROR: Could not pause job {self.name}")
+            return False
+
+    def unpause_job(self, logger):
+        if self.container is None:
+            return False
+        self.container.reload()
+        if not self.is_paused():
+            # print(f"WARN: Cannot unpause {self.name} because job is not paused.")
+            return False
+        try:
+            self.container.unpause()
+            logger.job_unpause(self.name)
+            self.start_time = time.time()
+            # print(f"Unpause {self}")
+            return True
+        except:
+            print(f"ERROR: Could not unpause job {self.name}")
+            return False 
+        
+    def finish_job(self, logger: slogger.SchedulerLogger):
+        if not self.container is None:
+            self.container.reload()
+        if not self.has_finished():
+            # print(f"WARN: Cannot finish {self.name} because job is not done yet.")
+            return False
+        elif self.container is None: # job has been removed already
+            return True
+        else:
+            try:
+                self.container.reload()
+                result = self.container.wait()
+                exit_code = result["StatusCode"]
+                logger.custom_event(self.name, f"exit code {exit_code}")
+                # print("Try to remove")
+                self.container.remove()
+                if exit_code != 0:
+                    print(f"ERROR: The following container exited with code {exit_code}\n{self}\nContainer stats: {result}")
+                    exit(-1)
+                logger.job_end(self.name)
+                logger.custom_event(self.name, f"runtime (s) {self.runtime}")
+                self.runtime = self.runtime + (time.time() - self.start_time)
+                self.container = None
+                self.has_finished_ = True
+                return True
+            except:
+                print(f"ERROR: Could not finish job {self.name}")
+                return False
+
+    def update_cores(self, cpu_set, logger):
+        if self.container is None:
+            return False
+        self.container.reload()
+        if self.container is None or self.has_finished():
+            # print(f"WARN: Cannot update {self.name} because job is not running.")
+            return False
+
+        self.container.update(cpuset_cpus=",".join([str(c) for c in cpu_set]))
+        logger.update_cores(self.name, [str(c) for c in cpu_set])
+        self.cores = cpu_set
+        # print(f"Update cores {self}")
+        return True
+    
+    def remove_container(self, force=False):
+        if not self.container is None:
+            self.container.remove(force=force)
+            self.container = None
+        
+
+class CannealJob(BatchJob):
+    name = slogger.Job.CANNEAL
+    num_threads = 4 # TODO
+    image = "anakli/cca:parsec_canneal"
+    command = f"./run -a run -S parsec -p canneal -i native -n {num_threads}"
+
+class BlackscholesJob(BatchJob):
+    name = slogger.Job.BLACKSCHOLES
+    num_threads = 2 # TODO
+    image = "anakli/cca:parsec_blackscholes"
+    command = f"./run -a run -S parsec -p blackscholes -i native -n {num_threads}"
+
+class FerretJob(BatchJob):
+    name = slogger.Job.FERRET
+    num_threads = 4 # TODO
+    image = "anakli/cca:parsec_ferret"
+    command = f"./run -a run -S parsec -p ferret -i native -n {num_threads}"
+
+class FreqmineJob(BatchJob):
+    name = slogger.Job.FREQMINE
+    num_threads = 4 # TODO
+    image = "anakli/cca:parsec_freqmine"
+    command = f"./run -a run -S parsec -p freqmine -i native -n {num_threads}"
+
+class DedupJob(BatchJob):
+    name = slogger.Job.DEDUP
+    num_threads = 4 # TODO
+    image = "anakli/cca:parsec_dedup"
+    command = f"./run -a run -S parsec -p dedup -i native {num_threads}"
+
+class RadixJob(BatchJob):
+    name = slogger.Job.RADIX
+    num_threads = 2 # TODO
+    image = "anakli/cca:splash2x_radix"
+    command = f"./run -a run -S splash2x -p radix -i native -n {num_threads}"
+
+class VipsJob(BatchJob):
+    name = slogger.Job.VIPS
+    num_threads = 2 # TODO
+    image = "anakli/cca:parsec_vips"
+    command = f"./run -a run -S parsec -p vips -i native -n {num_threads}"
+
+
+class MemcachedProcess(object):
+    pid = []
+    cores = 0
+    process_name = "memcache"
+
+    def __init__(self, cores):
+        self.get_pid()
+        self.cores = cores
+
+    def get_pid(self):
+        if len(self.pid) == 0:
+            for proc in psutil.process_iter():
+                if self.process_name in proc.name():
+                    self.pid.append(proc.pid)
+        return self.pid
+
+    def update_memcached_cores(self, logger, cores, enable_log=True):
+        if cores == self.cores:
+            return
+        if len(self.pid) == 0:
+            print(f"ERROR: Could not update memcached cores because pid is None")
+            return False
+        # print(f'Update memcache (pid: {self.pid}) CPUs to {cores}')
+        for pid in self.pid:
+            command = f'sudo taskset -a -cp {",".join([str(c) for c in cores])} {pid}'
+            subprocess.run(command.split(" "), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        if enable_log:
+            logger.update_cores(slogger.Job.MEMCACHED, [str(c) for c in cores])
+        self.cores = cores
+        return True
+    
+    def __str__(self):
+        return f"memcached: pid = {self.pid}, cores = {self.cores}"
+
+
+class Scheduler(object):
+
+    def __init__(self, memcached: MemcachedProcess, small_jobs: list[BatchJob], large_jobs: list[BatchJob]):
+        self.docker_client = docker.from_env()
+        self.logger = None
+        self.memcached = memcached
+        self.small_jobs = small_jobs
+        self.large_jobs = large_jobs
+        self.memcached.get_pid()
+        self.start_time = 0.0
+
+    def get_all_jobs(self) -> list[BatchJob]:
+        return self.small_jobs + self.large_jobs
+    
+    #CPU usage for memcache process
+    def get_pid_cpu(self):
+        pid = self.memcached.pid[0]
+        process = psutil.Process(pid)
+        cpu_usage = process.cpu_percent(interval=None)
+        return cpu_usage
+
+    # CPU usage per core
+    def get_cpu_usage(self):
+        return psutil.cpu_percent(interval=None, percpu=True)
+    
+    def check_all_jobs_finished(self):
+        return all(j.has_finished() for j in self.get_all_jobs())
+
+    def remove_finished_jobs(self):
+        for job in self.get_all_jobs():
+            if job.is_newly_finished():
+                job.finish_job(self.logger)
+
+    def print_job_status(self):
+        for job in self.get_all_jobs():
+            print(f"{job.name} - {job.get_status()}")
+
+    def update_cores_all_jobs(self, cpu_set):
+        if cpu_set == self.job_cores:
+            return
+        self.job_cores = cpu_set
+        for job in self.get_all_jobs():
+            job.update_cores(cpu_set, self.logger)
+    
+    def remove_all_jobs(self, force=False):
+        for job in self.get_all_jobs():
+            job.remove_container(force=force)
+
+
+    def start_run(self):
+        # start with 2 memcached cores in case there is high load in the beginning
+        self.memcached.update_memcached_cores(self.logger, [0,1], enable_log=False)
+        self.get_pid_cpu()
+        self.get_cpu_usage()
+        time.sleep(2)
+        print("Start scheduler")
+        self.logger = slogger.SchedulerLogger()
+        self.logger.job_start(slogger.Job.MEMCACHED, ["0","1"], 2)
+        self.job_cores = [2,3]
+        self.start_or_unpause_job(self.small_jobs)
+        self.start_time = time.time()
+
+    def end_run(self):
+        runtime = time.time() - self.start_time
+        self.remove_finished_jobs()
+        self.logger.custom_event(slogger.Job.SCHEDULER, f"runtime (s) {runtime}")
+        self.logger.end()
+        print("All jobs have finished. Force removing all containers in 10s...")
+        time.sleep(10)
+        self.remove_all_jobs(force=True)
+
+    def reload_all_jobs(self):
+        for j in self.get_all_jobs():
+            j.reload_stats()
+
+    def can_run_one_job(self, jobs: list[BatchJob]) -> bool:
+        return len([j for j in jobs if j.is_running() or j.is_new() or j.is_paused()]) > 0
+
+    def ensure_one_job_runs(self, jobs: list[BatchJob]):
+        running_jobs = [j for j in jobs if j.is_running()]
+        if len(running_jobs) == 0:
+            return self.start_or_unpause_job(jobs)
+        return True
+
+    def start_or_unpause_job(self, job: list[BatchJob]):
+        paused_jobs = [j for j in job if j.is_paused()]
+        if len(paused_jobs) > 0:
+            paused_jobs[0].unpause_job(self.logger)
+            return True
+        else:
+            new_jobs = [j for j in job if j.is_new()]
+            if len(new_jobs) > 0:
+                new_jobs[0].start_job(self.docker_client, self.job_cores, self.logger)
+                return True
+        return False
+    
+    def pause_all_jobs(self, jobs: list[BatchJob]):
+        running_jobs = [j for j in jobs if j.is_running()]
+        for j in running_jobs:
+            j.pause_job(self.logger)
+        return len(running_jobs)
+        
+
+    # main method!
+    def run(self):
+        self.start_run()
+
+        count_cpu_between_thresholds = 0
+
+        while True:
+            self.reload_all_jobs()
+            if self.check_all_jobs_finished():
+                break
+
+            self.remove_finished_jobs()
+
+            cpu_usage = self.get_cpu_usage()
+            cpu_usage_memcached = sum([cpu_usage[i] for i in memcached.cores])
+            print(f"CPU utilization = {cpu_usage}")
+            print(f"memcached CPU utilization = {cpu_usage_memcached}")
+            print(self.memcached)
+
+            job_string = "\n".join([str(j) for j in self.get_all_jobs()])
+            print(job_string)
+
+            if cpu_usage_memcached > THRESHOLD_HIGH:
+                self.memcached.update_memcached_cores(self.logger, [0,1])
+                self.update_cores_all_jobs([2,3])
+                count_cpu_between_thresholds = 0
+            elif cpu_usage_memcached < THRESHOLD_LOW or count_cpu_between_thresholds > 5:
+                self.memcached.update_memcached_cores(self.logger, [0])
+                self.update_cores_all_jobs([1,2,3])
+                count_cpu_between_thresholds = 0
+            else: 
+                count_cpu_between_thresholds = count_cpu_between_thresholds + 1
+
+            if len(self.memcached.cores) == 1:
+                # run a large job if possible, otherwise run a small job
+                if self.can_run_one_job(self.large_jobs):
+                    self.pause_all_jobs(self.small_jobs)
+                    time.sleep(0.1)
+                    self.ensure_one_job_runs(self.large_jobs) 
+                else:
+                    self.ensure_one_job_runs(self.small_jobs)
+                    
+            elif len(self.memcached.cores) == 2:
+                # only run a small job
+                self.pause_all_jobs(self.large_jobs)
+                time.sleep(0.1)
+                self.ensure_one_job_runs(self.small_jobs)
+                
+                
+            self.reload_all_jobs()
+            if self.check_all_jobs_finished():
+                break
+            self.get_cpu_usage()
+            time.sleep(SLEEP_TIME)
+
+        self.end_run()
+
+
+memcached = MemcachedProcess([])
+pids = memcached.get_pid()
+if len(pids) != 1:
+    print(f"\nUnexpected number of memcached processes. Expected 1, found {len(pids)} ({pids}). Abort!")
+    exit(-1)
+
+scheduler = Scheduler(memcached, 
+                      small_jobs=[BlackscholesJob(), RadixJob(), VipsJob(), FreqmineJob()],
+                      large_jobs=[FerretJob(), DedupJob(), CannealJob()])
+                    #   small_jobs=[BlackscholesJob(), RadixJob(), CannealJob(), VipsJob(), DedupJob()],
+                    #   large_jobs=[FerretJob(), FreqmineJob()])
+try:
+    scheduler.run()
+except Exception as e:
+    print(e)
+    print(f"\nUnexpected error. Remove all containers and abort.")
+    scheduler.remove_all_jobs(force=True)
+    exit(-1)
+
+print(f"\nScheduler terminated successfully!")
+    
+        
+    
+        
+    
+
--- task4/scheduler/archive/question4-2-results-scheduler2-17-122905/plot.py
+++ task4/scheduler/archive/question4-2-results-scheduler2-17-122905/plot.py
@@ -3,7 +3,6 @@ import matplotlib.dates as mdates
 import datetime
 import numpy as np
 
-# log.txt
 with open('log.txt') as f:
     logs = f.readlines()
 
@@ -11,7 +10,7 @@ with open('log.txt') as f:
 with open('measure.txt') as f:
     measures = f.readlines()
 
-# parse log.txt
+# log.txt
 scheduler_start_time = None
 scheduler_end_time = None
 memcached_cores = []
@@ -45,7 +44,7 @@ for log in logs:
         job = parts[2]
         batch_jobs[job].append((timestamp, 'end'))
 
-# parse measure.txt
+# 解析measure.txt
 intervals = int(measures[0].split('=')[1].strip())
 timestamp_start = int(measures[1].split(':')[1].strip())
 timestamp_end = int(measures[2].split(':')[1].strip())
@@ -53,6 +52,10 @@ timestamp_end = int(measures[2].split(':')[1].strip())
 p95_latency = []
 qps = []
 interval_duration = (timestamp_end - timestamp_start) / intervals / 1000  # Convert to seconds
+print(timestamp_start)
+print(datetime.datetime.fromtimestamp(timestamp_start/1000.0)-datetime.timedelta(hours=2))
+print(datetime.timedelta(seconds=2 * interval_duration))
+print(scheduler_start_time)
 
 for measure in measures[4:]:
     if measure.startswith('read'):
@@ -60,51 +63,40 @@ for measure in measures[4:]:
         p95_latency.append(float(parts[12]))
         qps.append(float(parts[16]))
 
-# align
-time_axis = [scheduler_start_time + datetime.timedelta(seconds=i * interval_duration) for i in range(intervals)]
 
-#time_axis = [datetime.datetime.fromtimestamp(timestamp_start/1000.0)-datetime.timedelta(hours=2) + datetime.timedelta(seconds=i * interval_duration) for i in range(intervals)]
+time_axis = [datetime.datetime.fromtimestamp(timestamp_start/1000.0)-datetime.timedelta(hours=2) + datetime.timedelta(seconds=i * interval_duration) for i in range(intervals)]
 
 time_axis_start_time = scheduler_start_time
-time_axis_end_time = scheduler_end_time
 time_axis = [(t - time_axis_start_time).total_seconds() for t in time_axis]
 
-# A: p95 latency QPS
+# plot A
 fig, (axA, axB, axC) = plt.subplots(3, 1, sharex=True, figsize=(12, 18))
 
 axA.plot(time_axis, [p / 1000 for p in p95_latency], color='blue', label='p95 Latency (ms)', linewidth=2)
 axA.axhline(y=1, color='r', linestyle='--', label='Target 1ms Latency')
 axA.set_ylabel('p95 Latency (ms)')
-axA.set_ylim(0, 2.5)
+axA.set_ylim(bottom=0)
 axA.legend(loc='upper left')
 
 axA2 = axA.twinx()
 axA2.fill_between(time_axis, 0, [q / 1000 for q in qps], color='green', alpha=0.3, label='QPS (kQPS)')
 axA2.set_ylabel('QPS (kQPS)')
-axA2.set_ylim(0, 100)
 axA2.legend(loc='upper right')
 
-# B: memcached CPU QPS
+# plot B
 core_times = [(t[0] - time_axis_start_time).total_seconds() for t in memcached_cores]
 core_values = [t[1] for t in memcached_cores]
 
-
-for i in range(len(core_times) - 1):
-    axB.plot([core_times[i], core_times[i + 1]], [core_values[i], core_values[i]], color='orange')
-axB.plot([core_times[-1], (time_axis_end_time - time_axis_start_time).total_seconds()], [core_values[-1], core_values[-1]], color='orange')
-
+axB.plot(core_times, core_values, color='orange', label='Memcached CPU Cores')
 axB.set_ylabel('Memcached CPU Cores')
-axB.set_ylim(0, 4)
-axB.set_yticks(range(0, 5))  
 axB.legend(loc='upper left')
 
 axB2 = axB.twinx()
 axB2.fill_between(time_axis, 0, [q / 1000 for q in qps], color='green', alpha=0.3, label='QPS (kQPS)')
 axB2.set_ylabel('QPS (kQPS)')
-axB2.set_ylim(0, 100)
 axB2.legend(loc='upper right')
 
-# C: batch job
+# plot C
 job_colors = {
     'blackscholes': '#CCA000', 'canneal': '#CCCCAA', 'dedup': '#CCACCA', 
     'ferret': '#AACCCA', 'freqmine': '#0CCA00', 'radix': '#00CCA0', 'vips': '#CC0A00'
@@ -113,21 +105,19 @@ job_colors = {
 for job, events in batch_jobs.items():
     for i in range(len(events)):
         start_event = events[i]
-        if start_event[1] in ['start', 'unpause']:
-            for j in range(i + 1, len(events)):
+        if start_event[1] == 'start':
+            for j in range(i+1, len(events)):
                 end_event = events[j]
-                if end_event[1] in ['pause', 'end']:
+                if end_event[1] in ['pause', 'unpause', 'end']:
                     start_time_sec = (start_event[0] - time_axis_start_time).total_seconds()
                     end_time_sec = (end_event[0] - time_axis_start_time).total_seconds()
                     axC.barh(job, end_time_sec - start_time_sec, left=start_time_sec, color=job_colors[job], edgecolor='black')
-                    break
+                    if end_event[1] == 'end':
+                        break
 
 axC.set_yticks(list(batch_jobs.keys()))
 axC.set_xlabel('Time (s)')
 axC.set_ylabel('Batch Jobs')
 axC.grid(True)
-
-
-axC.set_xlim(0, (time_axis_end_time - time_axis_start_time).total_seconds())
 
 plt.show()
